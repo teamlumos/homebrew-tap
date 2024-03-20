4c4af6a6b2b9be45ccb47b49b78b5a393e916a43
Brian Malehorn <brian@lumos.com>
2024-03-20 14:20:21 -0700

    fix sha256sum again removing typo

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -3,7 +3,7 @@ class Lumos < Formula
     desc "Lumos CLI"
     homepage "https://example.com"
     url "https://github.com/teamlumos/lumos-cli-releases/releases/download/0.4.0/lumos"
-    sha256 "0bdea86bb74e090a00533a13f111728951746389c28094a1246b5596c2a5e72arm"  # Replace with the SHA256 checksum of the binary
+    sha256 "0bdea86bb74e090a00533a13f111728951746389c28094a1246b5596c2a5e72a"  # Replace with the SHA256 checksum of the binary
 
     def install
         puts("hello from lumos install")
