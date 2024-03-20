039b046be7aa7d30aa02c96b7a71a2e56773c01c
Brian Malehorn <brian@lumos.com>
2024-03-20 14:24:50 -0700

    add version

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -4,6 +4,7 @@ class Lumos < Formula
     homepage "https://example.com"
     url "https://github.com/teamlumos/lumos-cli-releases/releases/download/0.4.0/lumos"
     sha256 "0bdea86bb74e090a00533a13f111728951746389c28094a1246b5596c2a5e72a"  # Replace with the SHA256 checksum of the binary
+    version "0.4.0"
 
     def install
         puts("hello from lumos install")
