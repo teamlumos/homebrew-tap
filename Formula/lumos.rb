f83aa52b6e4412cc2872fcf368b03b64caa907dd
Niamh Durfee <niamh@lumos.com>
2024-03-20 21:09:26 -0700

    remove install block

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -6,10 +6,6 @@ class Lumos < Formula
     sha256 "85f6bbaa59ec3e8095ccb0fb5e6baa58a9802c5d112a4a8404cf83ce59a2c927"
     version "0.5.1"
 
-    def install
-        bin.install "lumos.tar.gz"
-    end
-
     test do
     # Add test code here if needed
     end
