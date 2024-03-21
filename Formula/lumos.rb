801c44208a1907d15f999693d323d1650196b90d
Niamh Durfee <niamh@lumos.com>
2024-03-20 21:11:57 -0700

    revert removing install block

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -6,7 +6,7 @@ class Lumos < Formula
     sha256 "85f6bbaa59ec3e8095ccb0fb5e6baa58a9802c5d112a4a8404cf83ce59a2c927"
     version "0.5.1"
 
-    test do
-    # Add test code here if needed
+    def install
+        bin.install "lumos"
     end
 end
