ccf87f345c5bbdd513fe740359cc439fff1299af
Niamh Durfee <niamh@lumos.com>
2024-03-20 21:33:11 -0700

    Change back to binary

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -2,7 +2,7 @@ class Lumos < Formula
 
     desc "Lumos CLI"
     homepage "https://app.lumosidentity.com"
-    url "https://github.com/teamlumos/lumos-cli-releases/releases/download/0.5.1/lumos.tar.gz"
+    url "https://github.com/teamlumos/lumos-cli-releases/releases/download/0.5.1/lumos"
     sha256 "95c676e838f91c534824ae957f00bcc89de733a375c48ca9473260c69bfa2962"
     version "0.5.1"
 
