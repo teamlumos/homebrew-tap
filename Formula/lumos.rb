dabbce8320b220a31cd2070f04e35f4d2d20a75d
Niamh Durfee <niamh@lumos.com>
2024-03-20 21:02:44 -0700

    Update SHA

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -3,7 +3,7 @@ class Lumos < Formula
     desc "Lumos CLI"
     homepage "https://app.lumosidentity.com"
     url "https://github.com/teamlumos/lumos-cli-releases/releases/download/0.5.1/lumos.tar.gz"
-    sha256 "885f6bbaa59ec3e8095ccb0fb5e6baa58a9802c5d112a4a8404cf83ce59a2c92"
+    sha256 "85f6bbaa59ec3e8095ccb0fb5e6baa58a9802c5d112a4a8404cf83ce59a2c927"
     version "0.5.1"
 
     def install
