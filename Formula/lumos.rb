f8f473dd53a411f24f1c51664a4d29f87cb92615
Niamh Durfee <niamh@lumos.com>
2024-03-20 20:58:54 -0700

    Update lumos from 0.5.0 to 0.5.1

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -2,12 +2,11 @@ class Lumos < Formula
 
     desc "Lumos CLI"
     homepage "https://app.lumosidentity.com"
-    url "https://github.com/teamlumos/lumos-cli-releases/releases/download/0.5.0/lumos"
+    url "https://github.com/teamlumos/lumos-cli-releases/releases/download/0.5.1/lumos.tar.gz"
     sha256 "8c903d6481d103f0d8ce9071b1c35a861de8a9a059cf87dbb6de70ed9d07c9cf"  # Replace with the SHA256 checksum of the binary
-    version "0.5.0"
+    version "0.5.1"
 
     def install
-        puts("hello from lumos install")
         bin.install "lumos"
     end
 
