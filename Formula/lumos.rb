82918393e637479a77aab2606d365835c01e53ff
Niamh Durfee <niamh@lumos.com>
2024-03-22 12:59:53 -0700

    Update to 0.7.1

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -3,7 +3,7 @@ class Lumos < Formula
     desc "Lumos CLI"
     homepage "https://app.lumosidentity.com"
     url "https://github.com/teamlumos/lumos-cli-releases/releases/download/0.7.1/lumos.tar.gz"
-    sha256 ""
+    sha256 "772e05e6b0a1680637bdc5513b15a067773997df4b4a06efeeb8111d8046ce23"
     version "0.7.1"
 
     def install
