7c72824bc64fa866b7159ff42778a05268b4c760
Niamh Durfee <niamh@lumos.com>
2024-03-22 12:58:11 -0700

    Update lumos from 0.7.0 to 0.7.1

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -2,9 +2,9 @@ class Lumos < Formula
 
     desc "Lumos CLI"
     homepage "https://app.lumosidentity.com"
-    url "https://github.com/teamlumos/lumos-cli-releases/releases/download/0.7.0/lumos.tar.gz"
-    sha256 "8e5095020d83e1b016aa4b2b149793c0c4067bda27bf6ce9e704b57c32f79797"
-    version "0.7.0"
+    url "https://github.com/teamlumos/lumos-cli-releases/releases/download/0.7.1/lumos.tar.gz"
+    sha256 ""
+    version "0.7.1"
 
     def install
         libexec.install Dir["*"]
