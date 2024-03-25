da8a29fa3e278df9caf4be4970dfd6e070263c76
runner <runner@Mac-1711396527904.local>
2024-03-25 20:09:40 +0000

    Update lumos from 0.7.1 to 0.7.2

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -2,9 +2,9 @@ class Lumos < Formula
 
     desc "Lumos CLI"
     homepage "https://app.lumosidentity.com"
-    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.7.1/lumos.tar.gz"
-    sha256 "b3eaa450b04c7c10048f081dff947f7d17c2831d412cf520d2c346069ae15afe"
-    version "0.7.1"
+    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.7.2/lumos.tar.gz"
+    sha256 ""
+    version "0.7.2"
 
     def install
         libexec.install Dir["*"]
