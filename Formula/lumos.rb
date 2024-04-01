bfdb09e661cc21d42f9209212f5e139e4dc31583
Niamh Durfee <niamh@lumos.com>
2024-04-01 14:24:59 -0700

    Update lumos.rb - add SHA

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -3,7 +3,7 @@ class Lumos < Formula
     desc "Lumos CLI"
     homepage "https://app.lumosidentity.com"
     url "https://github.com/teamlumos/homebrew-tap/releases/download/0.8.2/lumos.tar.gz"
-    sha256 ""
+    sha256 "a7b7deeb6dcd5f26bea8e39385b63641daa5c179c230b4784f146e6c1acc339e"
     version "0.8.2"
 
     def install
