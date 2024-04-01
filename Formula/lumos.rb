b242cbdab216eae28427ebb63958e2580aa49488
runner <runner@Mac-1712005417957.local>
2024-04-01 21:23:14 +0000

    Update lumos from 0.7.2 to 0.8.2

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -2,9 +2,9 @@ class Lumos < Formula
 
     desc "Lumos CLI"
     homepage "https://app.lumosidentity.com"
-    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.7.2/lumos.tar.gz"
+    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.8.2/lumos.tar.gz"
     sha256 ""
-    version "0.7.2"
+    version "0.8.2"
 
     def install
         libexec.install Dir["*"]
