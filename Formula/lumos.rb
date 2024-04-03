ccdae69ce250c0b4406759e74eb7b5636f7061fd
runner <runner@Mac-1712172411163.local>
2024-04-03 19:40:13 +0000

    Update lumos from 0.8.2 to 0.8.3

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -2,9 +2,9 @@ class Lumos < Formula
 
     desc "Lumos CLI"
     homepage "https://app.lumosidentity.com"
-    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.8.2/lumos.tar.gz"
-    sha256 "a7b7deeb6dcd5f26bea8e39385b63641daa5c179c230b4784f146e6c1acc339e"
-    version "0.8.2"
+    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.8.3/lumos.tar.gz"
+    sha256 "cdad87edec7d6fcaf1f546f666a53bee164d31d2d0cb92c9bb9c2ffce2f06e8b"
+    version "0.8.3"
 
     def install
         libexec.install Dir["*"]
