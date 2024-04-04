2d5a5d4e991fd2a8a3327349172b5db2422c0329
Niamh Durfee <niamh@lumos.com>
2024-04-04 05:48:26 -0700

    Update lumos.rb
    Update homepage

--- Formula/lumos.rb
+++ Formula/lumos.rb
@@ -1,7 +1,7 @@
 class Lumos < Formula
 
     desc "Lumos CLI"
-    homepage "https://app.lumosidentity.com"
+    homepage "https://github.com/teamlumos/homebrew-tap"
     url "https://github.com/teamlumos/homebrew-tap/releases/download/0.8.3/lumos.tar.gz"
     sha256 "cdad87edec7d6fcaf1f546f666a53bee164d31d2d0cb92c9bb9c2ffce2f06e8b"
     version "0.8.3"
