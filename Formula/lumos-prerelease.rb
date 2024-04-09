518ffcd23478c4c120e4a1f6b7670f1dcb60e273
runner <runner@Mac-1712629162016.local>
2024-04-09 02:25:04 +0000

    Update lumos from 0.8.3 to 0.9.0 (prerelease)

--- Formula/lumos-prerelease.rb
+++ Formula/lumos-prerelease.rb
@@ -3,7 +3,7 @@ class LumosPrerelease < Formula
     desc "Lumos CLI"
     homepage "https://github.com/teamlumos/homebrew-tap"
     url "https://github.com/teamlumos/homebrew-tap/releases/download/0.9.0/lumos.tar.gz"
-    sha256 "dd019c37816119e941a0a4af924baa4a3a58845955f3f423f4adbb56a9871234"
+    sha256 "5e7e3a147c3cbb9bc1beceac57adb2b10da104c04df0a59107d2d5e49bd06534"
     version "0.9.0"
 
     def install
