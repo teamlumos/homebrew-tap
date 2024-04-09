bbd64d5ebb8a726e97a20c0a40615bf087eb96d8
Niamh Durfee <niamh@lumos.com>
2024-04-09 09:27:01 -0700

    Update lumos-prerelease.rb

--- Formula/lumos-prerelease.rb
+++ Formula/lumos-prerelease.rb
@@ -2,9 +2,9 @@ class LumosPrerelease < Formula
 
     desc "Lumos CLI"
     homepage "https://github.com/teamlumos/homebrew-tap"
-    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.9.0-prerelease/lumos.tar.gz"
+    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.9.1-prerelease/lumos.tar.gz"
     sha256 "f2b9e19e2cf1eb2a99d56246f4a38dc1a5fa21622fccee45a410b5425c4a37a4"
-    version "0.9.0"
+    version "0.9.1"
 
     def install
         libexec.install Dir["*"]
