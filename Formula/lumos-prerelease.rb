20f04b11966d81aaa18b183fbaaa4ebc29b06108
Niamh Durfee <niamh@lumos.com>
2024-04-08 19:28:04 -0700

    Update lumos-prerelease.rb

--- Formula/lumos-prerelease.rb
+++ Formula/lumos-prerelease.rb
@@ -2,7 +2,7 @@ class LumosPrerelease < Formula
 
     desc "Lumos CLI"
     homepage "https://github.com/teamlumos/homebrew-tap"
-    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.9.0/lumos.tar.gz"
+    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.9.0-prerelease/lumos.tar.gz"
     sha256 "5e7e3a147c3cbb9bc1beceac57adb2b10da104c04df0a59107d2d5e49bd06534"
     version "0.9.0"
 
