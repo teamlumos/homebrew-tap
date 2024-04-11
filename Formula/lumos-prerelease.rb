045240354b535317367774670ddcceef594c6165
runner <runner@Mac-1712869364995.local>
2024-04-11 21:06:35 +0000

    Update lumos from 0.9.3 to 1.0.0 (1.0.0-prerelease)

--- Formula/lumos-prerelease.rb
+++ Formula/lumos-prerelease.rb
@@ -2,9 +2,9 @@ class LumosPrerelease < Formula
 
     desc "Lumos CLI"
     homepage "https://github.com/teamlumos/homebrew-tap"
-    url "https://github.com/teamlumos/homebrew-tap/releases/download/0.9.3-prerelease/lumos.tar.gz"
-    sha256 "4fc9efe8fd73b9f6d7f506f2387302507205fce31fdc86d970b8cc25aebaff38"
-    version "0.9.3"
+    url "https://github.com/teamlumos/homebrew-tap/releases/download/1.0.0-prerelease/lumos.tar.gz"
+    sha256 "eff744a67515a67573c759b50d65744de5fa3be622d16f6cc9a2e9687693b2d6"
+    version "1.0.0"
 
     def install
         libexec.install Dir["*"]
