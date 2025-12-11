779cb337a006d958a327b939e5d4113d6cb33158
Jason D'Amour <jasondamour98@gmail.com>
2025-12-11 05:38:53 +0000

    ci: fix release pattern matching

--- Formula/lumos-prerelease.rb
+++ Formula/lumos-prerelease.rb
@@ -5,17 +5,17 @@ class LumosPrerelease < Formula
   license "MIT"
 
   on_macos do
-    url "https://github.com/teamlumos/lumos-cli/releases/download/2.1.2-prerelease/lumos-macos-arm64.tar.gz"
-    sha256 "415a9c5ecca9d43642d1d5f82be0f3ed98c3f42a175a569988e2124ba521ebb7"
+    url "https://github.com/teamlumos/lumos-cli/releases/download/v2.3.0/lumos-macos-arm64-v2.3.0.tar.gz"
+    sha256 "2ea0beeb41e2bd449abda1698663e5c20fbecac505734ee701345c4f2b1221d2"
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/teamlumos/lumos-cli/releases/download/2.1.2-prerelease/lumos-linux-amd64.tar.gz"
-      sha256 "x"
+      url "https://github.com/teamlumos/lumos-cli/releases/download/v2.3.0/lumos-linux-amd64-v2.3.0.tar.gz"
+      sha256 "e0a1fb4ce311844b0b874eb22a354a00a8ce2a477fac758aa687eb0f7e87cc52"
     elsif Hardware::CPU.arm?
-      url "https://github.com/teamlumos/lumos-cli/releases/download/2.1.2-prerelease/lumos-linux-arm64.tar.gz"
-      sha256 "x"
+      url "https://github.com/teamlumos/lumos-cli/releases/download/v2.3.0/lumos-linux-arm64-v2.3.0.tar.gz"
+      sha256 "3aebe52f1fa00ca25cab520524bea0e472d60dc3936caa2b379ed57c7fea74b2"
     end
   end
 
